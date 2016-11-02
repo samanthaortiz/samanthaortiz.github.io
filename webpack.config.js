d6a7d225ef405a514963ea761e31187a42b20f4d
Samantha Ortiz <samantha.ortiz@ncf.edu>
2016-11-02 14:12:41 -0400

    build(package.json): dev and production dependencies added

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'eval-source-map',
@@ -21,6 +22,19 @@ module.exports = {
     'failOnWarning': 'false',
     failOnError: 'false'
   },
+  plugins: [
+        new HtmlWebpackPlugin({
+          template: 'app/index.tpl.html',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
   module: {
     preLoaders: [
       {
@@ -39,6 +53,18 @@ module.exports = {
         test: /\.json?$/,
         loader: 'json'
       }
-    ]
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+      },
+      { 
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "url-loader?limit=10000&minetype=application/font-woff" 
+      },
+      { 
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "file-loader" 
+      }
+        ]
   }
 }
